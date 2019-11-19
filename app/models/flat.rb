fb7bc3ba4b6c31db31d72b0539b1e4bbd197c870
Daniel Gordon <d_gordon1@icloud.com>
2019-11-20 09:03:25 +1100

    add comment

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,5 @@
 class Flat < ApplicationRecord
   geocoded_by :address
+  # only if i change the value stored in the address column -> geocode
   after_validation :geocode, if: :will_save_change_to_address?
 end
